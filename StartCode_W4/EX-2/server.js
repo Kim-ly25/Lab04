54554f16c719509d4743506a3ea4a708a652ea45
Hak Kimly <kimlyhak25@gmail.com>
2025-05-30 10:34:28 +0700

    Update

--- StartCode_W4/EX-2/server.js
+++ StartCode_W4/EX-2/server.js
@@ -3,10 +3,13 @@ import articleRoutes from './routes/articleRoutes.js';
 import journalistRoutes from './routes/journalistRoutes.js';
 import categoryRoutes from './routes/categoryRoutes.js';
 import logger from './middleware/logger.js';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 
 const PORT = 3000;
+app.use(cors());
 
 app.use(logger);
 app.use(express.json());
@@ -14,6 +17,12 @@ app.use(express.json());
 app.use('/articles', articleRoutes);
 app.use('/journalists', journalistRoutes);
 app.use('/categories', categoryRoutes);
+
+app.get('/', (req, res) => {
+    res.send('API is running');
+});
+
+app.get('/favicon.ico', (req, res) => res.status(204).end());
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
